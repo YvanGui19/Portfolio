f772fe054f2b9efb9178820f612c86fdd2537de5
YvanGui19 <dixneufcode@gmail.com>
2020-02-04 16:45:09 +0100

    déroulement menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(event){
     console.log('load');
 
+//DÉROULEMENT DES SECTIONS VIA LE MENU----------------------------------------------------------------------------
+
+document.querySelectorAll('a[href^="#"]').forEach($anchor => {
+    $anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start' //scroll to top of the target element
+        });
+    });
+});
+
 })
